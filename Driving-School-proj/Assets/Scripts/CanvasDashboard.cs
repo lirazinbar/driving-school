d95bda90293fcc218190c0a2ed03f37309e9d51f
lirazinbar <hafifa0inbar@gmail.com>
2024-04-06 13:35:29 +0300

    feat: dasboard in canvas

---
+++ Driving-School-proj/Assets/Scripts/CanvasDashboard.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasDashboard : MonoBehaviour
+{
+    private CarController _carController;
+    public TMP_Text gearState; 
+
+    void Start()
+    {
+        gearState.SetText("P");
+
+    }
+
+    void FixedUpdate()
+    {
+        GearState currentGearState = _carController.GetGearState();
+        switch (currentGearState)
+        {
+            case GearState.Park:
+                gearState.SetText("P");
+                break;
+            case GearState.Drive:
+                gearState.SetText( "D");
+                break;
+            case GearState.Reverse:
+                gearState.SetText("R");
+                break;
+            default:
+                gearState.SetText("N/A"); // Not applicable or unknown state
+                break;
+        }
+    }
+}
