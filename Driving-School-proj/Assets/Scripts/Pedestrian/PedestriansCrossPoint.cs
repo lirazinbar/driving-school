7976087a99c375d1b409544c561a95f184e0e4e0
Eyal Politansky <eyalpolitansky@Eyals-MacBook-Pro.local>
2024-08-13 22:00:48 +0300

    Added instructor and fixed car roll over

---
+++ Driving-School-proj/Assets/Scripts/Pedestrian/PedestriansCrossPoint.cs
@@ -0,0 +1,31 @@
+using TrafficObjects.GiveWay;
+using UnityEngine;
+
+namespace Pedestrian
+{
+    public class PedestriansCrossPoint: MonoBehaviour
+    {
+        
+        [SerializeField] private GameObject pedestriansCloseSpawner;
+        [SerializeField] private GameObject pedestriansFarSpawner;
+        [SerializeField] private StopSign stopSign;
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Pedestrian"))
+            {
+                PedestrianController pedestrianController = other.GetComponent<PedestrianController>();
+                if (pedestrianController.SpawnerId() == pedestriansCloseSpawner.GetInstanceID())
+                {
+                   // Pedestrians starts crossing
+                   stopSign.PedestrianStartCrossing();
+                }
+                else if (pedestrianController.SpawnerId() == pedestriansFarSpawner.GetInstanceID())
+                {
+                    // Pedestrian finished crossing
+                    stopSign.PedestrianFinishCrossing();
+                }
+            }
+        }
+    }
+}
